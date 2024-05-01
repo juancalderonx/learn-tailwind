fe495c477692433f0106a1b814588dd017f6cea5
juancalderonx <juanescalderon12@gmail.com>
2024-04-30 23:04:15 -0500

    firts steps with tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'azul-claro': '#4D8AF0',
+        'azul-oscuro': '#2A4365',
+      }
+    },
+  },
+  plugins: [],
+}
